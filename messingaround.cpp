79937aa7b35b74498ff2f30ae926f8f18d20ba91
Rachel <112540274+Rtaylor18@users.noreply.github.com>
2022-09-21 20:48:27 -0500

    Add files via upload

---
+++ messingaround.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int userInput;
+bool running = true;
+int main(){
+    
+    while (running){
+        cout << "Enter a number between 1 and 10, press 0 to end program" << endl;
+        cin >> userInput;
+        if(userInput == 0)
+            running = false;
+        if (userInput > 10)
+            cout << "Invalid number, try again" << endl;
+        else 
+            for (int i = 0; i < userInput; i++)
+                cout << "Hello, World!" << endl;
+    }
+    
+    return 0;
+}
+
